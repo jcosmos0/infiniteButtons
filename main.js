e27107cbea88ddcf405d86f6024fc4284f720c77
PixelFireblade <joshuamoses@outlook.com>
2017-05-03 09:13:27 +1000

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ $ ( document ).ready(function(){
   
   
   
-  var link = ['', 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Patates.jpg', 'https://cdn.shopify.com/s/files/1/1017/2183/t/2/assets/live-preview-potato.png?4324246315360566414' ]
+  var link = ['', 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Patates.jpg', 'https://cdn.shopify.com/s/files/1/1017/2183/t/2/assets/live-preview-potato.png?4324246315360566414' ];
   $("#generatePotato").click(function() {
     var rand = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
     console.log(rand);
     $('#potatoImg').prepend('<img id="theImg" src=link[rand] />')
     
-  }
+  });
                        
     
 
