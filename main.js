049273649b4192aa1d49499994fe8f81cefbb375
PixelFireblade <joshuamoses@outlook.com>
2017-05-05 21:49:27 +1000

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $ ( document ).ready(function(){
   
   
   //var link = ['', 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Patates.jpg', 'https://cdn.shopify.com/s/files/1/1017/2183/t/2/assets/live-preview-potato.png?4324246315360566414' ];
-  $("#generatePotato").click(function() {
+  $("#potatoGenerate").click(function() {
     //var rand = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
     //console.log(rand);
     $("#potatoImg").append('<img src="http://i2.kym-cdn.com/entries/icons/original/000/011/269/potato.jpg" alt="someimage" style="width:604px;height:456px;"/>');
