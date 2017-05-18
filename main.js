2192c06e2ec451a8d4cf5dbb04270e5c35498969
PixelFireblade <joshuamoses@outlook.com>
2017-05-18 14:50:20 +1000

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ $ ( document ).ready(function(){
   
   
   var link = ['', 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Patates.jpg', 'https://cdn.shopify.com/s/files/1/1017/2183/t/2/assets/live-preview-potato.png?4324246315360566414','http://i2.kym-cdn.com/entries/icons/original/000/011/269/potato.jpg', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ4XgtyVr_1lIm1zt6nRE6_fhonFC98eqLN1K2evH4xveXAjKCJA3drK2Lp', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcREfOQzfG_-OIJDoKXRyIUfFlJtYQukIpJV8Z5LmugDFT4CdRO_', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTCD3fVNb0cYuoZDCJ598Wa6yHZAmB4hnKKzKxMl3H9kaYhMTW0', 'https://yt3.ggpht.com/-hRHTc1eQ-Yc/AAAAAAAAAAI/AAAAAAAAAAA/m0Hk6YuhxTM/s900-c-k-no-mo-rj-c0xffffff/photo.jpg', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQCW7hmimZGjVrnaZEXzYwJp4CRVJL7l26oHwXQ78jgnEisAdyk', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRDCPLDuntvHb2KUR7849WDLqiU7eUh4VxlphabkhtjcbZMt4px', 'http://www.potatoes.com/files/3113/4435/6702/03_NUTRITION_Background.jpg', 'http://blogs.app.com/food/files/2012/10/potato_1.jpg', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTLJ3aSpXes5I29rQ7bZanAxoIP8tvGmV7sGlxB0yT49Mw27HTB7g' ];
-  $("#potatoGenerate").click(function() {
-    var rand = Math.floor(Math.random() * (link.length + 1 - 1 + 1)) + 1;
-    console.log(rand);
-    console.log(link[rand])
-    $("#potatoImg").append('<center><img src="'+link[rand]+'" alt="A Potato" style="width:604px;height:456px;"/></center>');
+  $("#buttonOne").click(function() {
+    buttonClicked = 'click';
+    
+    
     
   });
+  while(buttonClicked==='click'){
+    $("#buttonOne2").append('<center><button type="button" id = "buttonOne">Click Me!</button></center>');
+    buttonClicked = 'no';
+    
+  }
                        
     
 
