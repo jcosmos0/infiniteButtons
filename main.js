faef50040abbce48ab3a44ff300ac6a8cde3b7d5
PixelFireblade <joshuamoses@outlook.com>
2017-05-19 21:43:24 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 $ ( document ).ready(function(){
-  buttonClicked = false;
   
    
  
   $("#buttonOne").click(function() {
-    buttonClicked = true;
-    
+    $("#buttonOne2").append('<center><button type="button" id = "buttonOne">Click Me!</button></center>');
     
     
   });
-  while(buttonClicked==='click'){
-    $("#buttonOne2").append('<center><button type="button" id = "buttonOne">Click Me!</button></center>');
-    buttonClicked = false;
-    
-  }
+ 
+  
                        
     
 
