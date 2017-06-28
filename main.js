7d15ca5bfcc48a844f8e9250db93843cdd050c62
PixelFireblade <joshuamoses@outlook.com>
2017-06-29 09:20:41 +1000

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ $ ( document ).ready(function(){
   
    
  
-  $("#buttonOne").click(function() {
-    $("#buttonOne2").append('<center><button type="button" id = "buttonOne">Click Me!</button></center>');
+  $("#buttonOne2").click(function() {
+    $("#buttonOne2").append('<center><button type="button" id = "buttonOne3">Click Me!</button></center>');
+    
+    
+  });
+  $("#buttonOne3").click(function() {
+    $("#buttonOne2").append('<center><button type="button" id = "buttonOne3">Click Me!</button></center>');
     
     
   });
@@ -18,7 +23,3 @@ $ ( document ).ready(function(){
 
 
 
-function buttonClick() {
-    $("#buttonOne2").append('<center><button type="button" id = "buttonOne">Click Me!</button></center>');
-  
-  }
