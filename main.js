ad3f131ea9dfa4ad0879391a72b075749854e6f7
PixelFireblade <joshuamoses@outlook.com>
2017-06-29 09:23:39 +1000

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $ ( document ).ready(function(){
   
    
  
-  $("#buttonOne2").click(function() {
+  $("#buttonOne").click(function() {
     $("#buttonOne2").append('<center><button type="button" id = "buttonOne3">Click Me!</button></center>');
     
     
